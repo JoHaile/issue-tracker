98fa2633902d24130ae322d0079720163a1de09b
John <yohannes.h93@gmail.com>
2025-07-12 04:43:45 +0300

    add the radix iu library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme accentColor="gold" grayColor="sand" radius="large">
+          {children}
+        </Theme>
       </body>
     </html>
   );
