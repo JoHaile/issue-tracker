9ea658975400a76d82634139afbc7530e0f194ce
John <yohannes.h93@gmail.com>
2025-07-20 01:39:10 +0300

    add the Active link feature

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,33 @@
+"use client";
+
 import React from "react";
 import { Flex, Text } from "@radix-ui/themes";
 import ChangeTheme from "@/lib/theme";
 import Link from "next/link";
 import { Bug } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 function NavBar() {
+  const path = usePathname();
+
   return (
     <Flex p="4" justify="between" className="border-b-1 border-b-gray-500">
       <div className="flex gap-7 items-center">
         <Link href="/">
           <Bug />
         </Link>
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/issues">Issues</Link>
+        <Link
+          href="/dashboard"
+          className={path === "/dashboard" ? "text-blue-300" : ""}
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/issues"
+          className={path === "/issues" ? "text-blue-300" : ""}
+        >
+          Issues
+        </Link>
       </div>
       <ChangeTheme />
     </Flex>
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Issue Page</div>;
+}
+
+export default page;
