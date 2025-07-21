19e57585218da3863abc7660afb0a895501d7a8d
John <yohannes.h93@gmail.com>
2025-07-21 22:55:50 +0300

    responsive layout done

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,51 @@
-import { Button, Heading } from "@radix-ui/themes";
+import { prisma } from "@/prisma/client";
+import { Button, Heading, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-function page() {
+async function page() {
+  const issues = await prisma.issue.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div>
-      <Heading>Issue Page</Heading>
-
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
+      <div className="p-4">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.RowHeaderCell>Issue</Table.RowHeaderCell>
+            <Table.RowHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.RowHeaderCell>
+            <Table.RowHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.RowHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="sm:hidden block">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 }
