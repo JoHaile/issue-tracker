163ac50cf3337126c3e3697d3a2538c58b7bd2c8
John <yohannes.h93@gmail.com>
2025-07-22 00:41:02 +0300

    added a loading skelton for all pages

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Flex, Skeleton } from "@radix-ui/themes";
 
 function loading() {
-  return <div>loading...</div>;
+  return (
+    <div className="max-w-4/5">
+      <Skeleton height="2rem" width="10rem" />
+      <Flex gap="4" py="3" align="center">
+        <Skeleton width="3rem" />
+
+        <Skeleton height="5rem" />
+      </Flex>
+      <Skeleton height="10rem" />
+    </div>
+  );
 }
 
 export default loading;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,17 @@
+import { Skeleton } from "@radix-ui/themes";
 import React from "react";
 
 function loading() {
-  return <div>loading...</div>;
+  return (
+    <div className="w-4/6">
+      <form className="space-y-4">
+        <Skeleton height="1rem" />
+        <Skeleton height="10rem" />
+
+        <Skeleton width="2rem" height="1rem" />
+      </form>
+    </div>
+  );
 }
 
 export default loading;
